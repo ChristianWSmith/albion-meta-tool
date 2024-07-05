2e7debfc5b22eac2864cb43a9c4db7d0e29e12bf
Christian Smith <smith.christian.william@gmail.com>
2024-07-05 08:27:43 -0400

    test push

--- logging.go
+++ logging.go
@@ -10,6 +10,7 @@ func initLogging() error {
 	log.SetFormatter(&logrus.JSONFormatter{})
 	logFile, err := os.OpenFile(config.LogFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
 	if err != nil {
+		log.Error("Failed to open log file: ", config.LogFile)
 		return err
 	}
 	log.SetOutput(logFile)
