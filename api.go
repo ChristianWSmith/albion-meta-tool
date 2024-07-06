068167440d174130c9ca436eb23a6ea630e43502
Christian Smith <smith.christian.william@gmail.com>
2024-07-05 21:56:49 -0400

    query all events for report

--- api.go
+++ api.go
@@ -6,18 +6,30 @@ import (
 	"net/http"
 )
 
+func generateReport() [][]string {
+
+	response := [][]string{}
+	events, err := queryAllEvents()
+	if err != nil {
+		log.Error("Failed to query all events: ", err)
+
+	}
+
+	for _, event := range events {
+		response = append(response, []string{fmt.Sprintf("%v", event)})
+	}
+
+	return response
+}
+
 // Handler function for the endpoint
 func reportHandler(w http.ResponseWriter, r *http.Request) {
 	// Create a response in CSV format
-	response := [][]string{
-		{"col1", "col2", "col3"},
-		{"val1", "val2", "val3"},
-		{"val4", "val5", "val6"},
-	}
+	response := generateReport()
 
 	// Encode and send the CSV response
 	w.Header().Set("Content-Type", "text/csv")
-	w.Header().Set("Content-Disposition", "attachment;filename=response.csv")
+	w.Header().Set("Content-Disposition", "attachment;filename=report.csv")
 	writer := csv.NewWriter(w)
 	for _, record := range response {
 		if err := writer.Write(record); err != nil {
